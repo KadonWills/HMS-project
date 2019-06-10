8fcc280a2be7ebc82676aeebff0949ec8fc0ab1d
KadonWills <kapolw@gmail.com>
2019-06-10 01:36:11 +0100

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>HMS</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #ffffff;
+                color: rgb(255, 53 , 53);
+                font-family: 'Raleway', sans-serif;
+                font-weight: bold;
+                height: 100vh;
+                margin: 0;
+                background-image: url("https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/FyKJcnp/two-professional-african-american-doctors-talk-while-using-their-tablets-on-a-white-background_rujiyriqe_thumbnail-full01.png");
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (!Route::has('login'))
+                <div class="top-right links">
+                    @if (!Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md" style="background: rgba(255, 255, 255, 0.7)">
+                    Hospital Management System
+                </div>
+
+                <div class="links">
+                 
+                </div>
+            </div>
+        </div>
+    </body>
+</html>

89171543d0e68394d9ab092cbee3e7cc6ae017c6
MNC-Aubin <mouafoaubin@gmail.com>
2019-06-14 22:13:32 +0100

    font-awesome added in public folder
    The library font-awesome have been added to the project.

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HMS  - @yield('title')</title>
 
-    <!-- Favicons -->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>HMS - @yield('title')</title>
+
+  <!-- Fontawesome -->
+  <link href="{{ asset('css/brands.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/solid.css') }}" rel="stylesheet">
   <!-- Bootstrap core CSS -->
   <link href="{{ asset('lib/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
   <!--external css-->
@@ -25,56 +28,58 @@
   <script src="{{ asset('js/common-scripts.js') }}" defer></script>
 
 </head>
+
 <body>
 
-    <section id="container">
+  <section id="container">
 
-        <!-- **********************************************************************************************************************************************************
+    <!-- **********************************************************************************************************************************************************
         TOP BAR CONTENT & NOTIFICATIONS
         *********************************************************************************************************************************************************** -->
     <!--header start-->
     <header class="header black-bg">
-            <div class="sidebar-toggle-box">
-              <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
-            </div>
-            <!--logo start-->
-            <a href="{{url('/')}}" class="logo"><b>RMS</b></a>
-            <!--logo end-->
-            
-            <div class="top-menu">
-              <ul class="nav pull-right top-menu">
-                <li><a class="logout" href="login.html">Logout</a></li>
-              </ul>
-            </div>
-          </header>
-          <!--header end-->
+      <div class="sidebar-toggle-box">
+        <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+      </div>
+      <!--logo start-->
+      <a href="{{url('/')}}" class="logo"><b>RMS</b></a>
+      <!--logo end-->
+
+      <div class="top-menu">
+        <ul class="nav pull-right top-menu">
+          <li><a class="logout" href="login.html">Logout</a></li>
+        </ul>
+      </div>
+    </header>
+    <!--header end-->
 
     <!-- **********************************************************************************************************************************************************
         MAIN SIDEBAR MENU
         *********************************************************************************************************************************************************** -->
     <!--sidebar start-->
-       @yield('sub-menu')      
-      <!--sidebar end-->
+    @yield('sub-menu')
+    <!--sidebar end-->
 
     <div class="container">
-        @yield('content')
+      @yield('content')
     </div>
 
-    
+
     <!--footer start-->
     <footer class="site-footer">
-        <div class="text-center">
-          <p>
-            &copy;<strong>2019</strong>. Software Engineering
-          </p>
-          <div class="credits">
-            <a href="url('/')'">Hospital Management System</a>
-          </div>
+      <div class="text-center">
+        <p>
+          &copy;<strong>2019</strong>. Software Engineering
+        </p>
+        <div class="credits">
+          <a href="url('/')'">Hospital Management System</a>
         </div>
-      </footer>
-      <!--footer end-->
+      </div>
+    </footer>
+    <!--footer end-->
 
-</section>
+  </section>
 
 </body>
+
 </html>
